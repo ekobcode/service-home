7c65efd6e5441abef401ba539a673cdc36a672f2
Eko Budiarto <61195513+ekobcode@users.noreply.github.com>
2022-10-19 01:18:28 +0700

    Update ServiceHomeApplication.java

--- src/main/java/servicehome/servicehome/ServiceHomeApplication.java
+++ src/main/java/servicehome/servicehome/ServiceHomeApplication.java
@@ -2,12 +2,13 @@ package servicehome.servicehome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class ServiceHomeApplication {
 
-	@RequestMapping("/home")
+	@GetMapping("/home")
 	public String home() {
 		return "Hello This is Home Page";
 	}
