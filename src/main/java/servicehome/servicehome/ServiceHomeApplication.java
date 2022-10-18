7b2a905c84792a9531740e8ac6c444a4f10b2e25
Eko Budiarto <61195513+ekobcode@users.noreply.github.com>
2022-10-19 01:12:33 +0700

    Initial commit

---
+++ src/main/java/servicehome/servicehome/ServiceHomeApplication.java
@@ -0,0 +1,19 @@
+package servicehome.servicehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+public class ServiceHomeApplication {
+
+	@RequestMapping("/home")
+	public String home() {
+		return "Hello This is Home Page";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/servicehome/servicehome/ServiceHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package servicehome.servicehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
